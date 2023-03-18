e6294cf29991482e6b80a311ee5c89c33478501d
Dor Truzman <dor.truzman@gmail.com>
2023-03-18 23:45:39 +0200

    Almost - uploading photo still doesn't work but it's a good progress :)

--- app/src/main/java/com/example/tripi/RegistrationFragment.java
+++ app/src/main/java/com/example/tripi/RegistrationFragment.java
@@ -1,12 +1,17 @@
 package com.example.tripi;
 
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +19,7 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.tripi.model.Model;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -21,6 +27,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 
+import java.io.IOException;
 import java.util.concurrent.Executor;
 
 /**
@@ -31,7 +38,9 @@ import java.util.concurrent.Executor;
 public class RegistrationFragment extends Fragment {
 
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    protected Uri imageUri;
     private FirebaseAuth mAuth;
+    ActivityResultLauncher<String> galleryLauncher;
 
     public RegistrationFragment() {
         // Required empty public constructor
@@ -41,6 +50,15 @@ public class RegistrationFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
+
+        galleryLauncher = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri result) {
+                if (result != null){
+                    imageUri = result;
+                }
+            }
+        });
     }
 
     @Override
@@ -48,6 +66,7 @@ public class RegistrationFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_registration, container, false);
+        View browseBtn = view.findViewById(R.id.profilePictureButton);
         View button = view.findViewById(R.id.registerButton);
         TextView failedRegistrationText = (TextView) view.findViewById(R.id.failedRegistration);
 
@@ -58,11 +77,15 @@ public class RegistrationFragment extends Fragment {
             createAccount(mail.getText().toString(), password.getText().toString(), dispName.getText().toString(), view1, failedRegistrationText);
         });
 
+        browseBtn.setOnClickListener((view2) -> {
+            galleryLauncher.launch("image/*");
+        });
+
         return view;
     }
 
     private void createAccount(String email, String password, String dispName, View view, TextView failedRegistration) {
-        if (email.isEmpty() || password.isEmpty() || dispName.isEmpty()) failedRegistration.setVisibility(View.VISIBLE);
+        if (email.isEmpty() || password.isEmpty() || dispName.isEmpty() || imageUri.toString().isEmpty()) failedRegistration.setVisibility(View.VISIBLE);
         else {
             mAuth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -70,9 +93,19 @@ public class RegistrationFragment extends Fragment {
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
                                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                                UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder().setDisplayName(dispName).setPhotoUri(Uri.parse("https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Israel-2013-Aerial_21-Masada.jpg/1200px-Israel-2013-Aerial_21-Masada.jpg")).build();
-                                user.updateProfile(profileUpdates);
-                                Navigation.findNavController(view).navigate(R.id.action_registrationFragment_to_tripListFragment);
+                                try {
+                                    // Not working
+                                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(MyApplication.getAppContext().getContentResolver(), imageUri);
+                                    Model.instance().uploadImage(user.getUid(), bitmap, url-> {
+                                        if (url != null) {
+                                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder().setDisplayName(dispName).setPhotoUri(Uri.parse(url)).build();
+                                            user.updateProfile(profileUpdates);
+                                            Navigation.findNavController(view).navigate(R.id.action_registrationFragment_to_tripListFragment);
+                                        }
+                                    });
+                                } catch (IOException e) {
+                                    failedRegistration.setVisibility(View.VISIBLE);
+                                }
                             } else {
                                 failedRegistration.setVisibility(View.VISIBLE);
                             }
